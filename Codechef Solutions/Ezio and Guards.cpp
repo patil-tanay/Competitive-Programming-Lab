6307fc0442b4ab290fda066c651b6c7c7269f2e3
Tanay <94126651+patil-tanay@users.noreply.github.com>
2022-08-24 15:22:10 +0530

    Problem Code: MANIPULATE

---
+++ Codechef Solutions/Ezio and Guards.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>=b)
+	    {
+	        cout<<"YES \n";
+	    }
+	    else
+	    {
+	        cout<<"NO \n";
+	    }
+	}
+	return 0;
+}
